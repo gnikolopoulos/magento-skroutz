8517293eaa2f1fa013e01ad06df5b26114caf431
George Nikolopoulos <georgen@interactive-design.gr>
2017-08-17 14:55:20 +0300

    New Init

--- app/code/local/ID/Feed/controllers/IndexController.php
+++ app/code/local/ID/Feed/controllers/IndexController.php
@@ -1,249 +1,252 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class ID_Feed_IndexController extends Mage_Core_Controller_Front_Action {
 
-  private $oProducts;
-  private $oProdudctIds;
-  private $oProductModel;
-  private $store_name;
-  private $xml_file_name;
-  private $xml_path;
-  private $file;
-  private $excluded;
-  private $xml;
-  private $xmlContents;
+	private $oProducts;
+	private $oProdudctIds;
+	private $oProductModel;
+	private $store_name;
+	private $xml_file_name;
+	private $xml_path;
+	private $file;
+	private $excluded;
+	private $xml;
+	private $xmlContents;
+	private $base_node;
 
-  private $attribute;
+	private $BadChars = array('"',"\r\n","\n","\r","\t");
+	private $ReplaceChars = array(""," "," "," ","");
 
-  private $BadChars = array('"',"\r\n","\n","\r","\t");
-  private $ReplaceChars = array(""," "," "," ","");
+	private $notAllowed = array('Νο', 'Όχι');
 
-  private $notAllowed = array('Νο', 'Όχι');
+	private function init()
+	{
+		$this->store_name = Mage::getStoreConfig('feed/feed/store_name');
+		$this->xml_file_name = Mage::getStoreConfig('feed/feed/xml_file_name');
+		$this->xml_path = Mage::getStoreConfig('feed/feed/feed_path');
+		$this->file = $this->xml_path . $this->xml_file_name;
 
-  private function init()
-  {
-    $this->store_name = Mage::getStoreConfig('feed/feed/store_name');
-    $this->xml_file_name = Mage::getStoreConfig('feed/feed/xml_file_name');
-    $this->xml_path = Mage::getStoreConfig('feed/feed/feed_path');
-    $this->file = $this->xml_path . $this->xml_file_name;
+		$this->show_outofstock = Mage::getStoreConfig('feed/collection/show_unavailable');
+		$this->excluded = explode(',', Mage::getStoreConfig('feed/collection/excluded_cats'));
 
-    $this->attribute = Mage::getStoreConfig('feed/collection/attribute');
+		$this->instock_msg = Mage::getStoreConfig('feed/messages/in_stock');
+		$this->nostock_msg = Mage::getStoreConfig('feed/messages/out_of_stock');
+		$this->backorder_msg = Mage::getStoreConfig('feed/messages/backorder');
 
-    $this->show_outofstock = Mage::getStoreConfig('feed/collection/show_unavailable');
-    $this->excluded = explode(',', Mage::getStoreConfig('feed/collection/excluded_cats'));
+		$this->base_url = Mage::app()->getStore(1)->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK);
+		$this->media_url = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA) . 'catalog/product';
+	}
 
-    $this->instock_msg = Mage::getStoreConfig('feed/messages/in_stock');
-    $this->nostock_msg = Mage::getStoreConfig('feed/messages/out_of_stock');
-    $this->backorder_msg = Mage::getStoreConfig('feed/messages/backorder');
-  }
+	public function indexAction() {
 
-  public function indexAction() {
+		$time_start = microtime(true);
 
-    $this->init();
+		$this->init();
+		$this->createXML();
+		$this->openXML();
 
-    $this->getProducts();
-    $this->createXML();
+		$this->base_node = $this->xml->getElementsByTagName('products')->item(0);
 
-    $this->openXML();
+		$this->getProducts();
 
-    $base_node = $this->xml->getElementsByTagName('products')->item(0);
+		$this->xml->formatOutput = true;
+		$this->xml->save($this->file);
 
-    foreach ($this->oProdudctIds as $iProduct) {
-      @set_time_limit(0);
+		echo 'XML Feed generated in: ' . number_format((microtime(true) - $time_start), 2) . ' seconds';
 
-      $oProduct = Mage::getModel('catalog/product');
-      $oProduct ->load($iProduct);
-      $stockItem = $oProduct->isAvailable();
-      $skroutz = $oProduct->getData( $this->attribute );
-      if($stockItem == 1 && $skroutz == 1) {
-        $p = $this->getProductData($iProduct);
+	}
 
-        $product = $this->xml->createElement("product");
-        $base_node->appendChild( $product );
+	private function createXML() {
+		$dom = new DomDocument("1.0", "utf-8");
+		$dom->formatOutput = true;
 
-        $product->appendChild ( $this->xml->createElement('id', $p['id']) );
-        $product->appendChild ( $this->xml->createElement('mpn', $p['mpn']) );
-        $product->appendChild ( $this->xml->createElement('manufacturer', $p['brand']) );
+		$root = $dom->createElement($this->store_name);
 
-        $name = $product->appendChild($this->xml->createElement('name'));
-        $name->appendChild($this->xml->createCDATASection( $p['title'] ));
+		$stamp = $dom->createElement('created_at', date('Y-m-d H:i') );
+		$root->appendChild($stamp);
 
-        $description = $product->appendChild($this->xml->createElement('description'));
-        $description->appendChild($this->xml->createCDATASection( $p['description'] ));
+		$nodes = $dom->createElement('products');
+		$root->appendChild($nodes);
 
-        $product->appendChild ( $this->xml->createElement('price', $p['price']) );
-        $product->appendChild ( $this->xml->createElement('link', $p['link']) );
-        $product->appendChild ( $this->xml->createElement('image', $p['image_link_large']) );
-        $product->appendChild ( $this->xml->createElement('InStock', $p['stock']) );
-        $product->appendChild ( $this->xml->createElement('Availability', $p['stock_descrip']) );
+		$nameAttribute = $dom->createAttribute('name');
+		$nameAttribute->value = Mage::app()->getStore()->getFrontendName();
+		$root->appendChild($nameAttribute);
 
-        $category = $product->appendChild($this->xml->createElement('category'));
-        $category->appendChild($this->xml->createCDATASection( $p['category'] ));
+		$urlAttribute = $dom->createAttribute('url');
+		$urlAttribute->value = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
+		$root->appendChild($urlAttribute);
 
-        $product->appendChild ( $this->xml->createElement('categoryid', $p['categoryid']) );
+		$dom->appendChild($root);
 
-        if( $p['color'] != '' && !in_array($p['color'], $this->notAllowed) ) {
-          $product->appendChild ( $this->xml->createElement('color', $p['color']) );
-        }
+		$dom->save($this->file);
+	}
 
-        if( isset($p['size']) && $p['size'] != '' ) {
-          $product->appendChild ( $this->xml->createElement('size', $p['size']) );
-        }
+	private function openXML() {
+		$this->xml = new DOMDocument();
+		$this->xml->formatOutput = true;
+		$this->xml->load($this->file);
+	}
 
-        $this->xml->formatOutput = true;
-        $this->xml->save($this->file);
+	private function getProducts() {
+		$this->oProducts = Mage::getModel('catalog/product')->getCollection();
+		$this->oProducts->addAttributeToFilter('status', 1); //enabled
+		$this->oProducts->addAttributeToFilter('visibility', 4); //catalog, search
+		$this->oProducts->addAttributeToFilter(
+			array(
+				array('attribute'=>'skroutz', 'eq' => '1'),
+			)
+		); //skroutz products only
+		$this->oProducts->addAttributeToSelect(['entity_id', 'skroutz','sku','name','manufacturer_value','final_price','short_description','url_path','small_image','color_value','type_id']);
+		if( !$this->show_outofstock ) {
+			$this->oProducts->joinField('qty',
+					'cataloginventory/stock_item',
+					'qty',
+					'product_id=entity_id',
+					'{{table}}.stock_id=1',
+					'left'
+				);
+			$this->oProducts->joinTable('cataloginventory/stock_item', 'product_id=entity_id', array('stock_status' => 'is_in_stock'));
+			$this->oProducts->addAttributeToFilter('stock_status', 1);
+		}
+		$this->oProducts->addFinalPrice();
 
-      } // endif
+		Mage::getSingleton('core/resource_iterator')->walk(
+			$this->oProducts->getSelect(),
+			array(array($this, 'productCallback')),
+			array('store_id' => $storeId)
+		);
+	}
 
-    } // endforeach
+	public function productCallback($args) {
+		$oProduct = Mage::getModel('catalog/product')->setData($args['row']);
 
-  }
+		$aCats = $this->getCategories($oProduct);
 
-  private function createXML() {
-    $dom = new DomDocument("1.0", "utf-8");
-    $dom->formatOutput = true;
+		$aData = array();
 
-    $root = $dom->createElement($this->store_name);
+		$aData['id'] = $oProduct->entity_id;
+		$aData['mpn'] = mb_substr($oProduct->sku,0,99,'UTF-8');
 
-    $stamp = $dom->createElement('created_at', date('Y-m-d H:i') );
-    $root->appendChild($stamp);
+		$aData['brand'] = strtoupper( @mb_substr($oProduct->manufacturer_value,0,99,'UTF-8') );
 
-    $nodes = $dom->createElement('products');
-    $root->appendChild($nodes);
+		$_finalPrice = $oProduct->final_price;
+		if( $_finalPrice >= 60 && $_finalPrice < 200 ) {
+			$aData['title'] = $aData['brand'] . ' ' . mb_substr($oProduct->name,0,299,'UTF-8') . ' - ' . $aData['mpn'] . ' (Πληρωμή με 2 άτοκες δόσεις)';
+		} elseif( $_finalPrice >= 200 ) {
+			$aData['title'] = $aData['brand'] . ' ' . mb_substr($oProduct->name,0,299,'UTF-8') . ' - ' . $aData['mpn'] . ' (Πληρωμή με 4 άτοκες δόσεις)';
+		} else {
+			$aData['title'] = $aData['brand'] . ' ' . mb_substr($oProduct->name,0,299,'UTF-8') . ' - ' . $aData['mpn'];
+		}
 
-    $nameAttribute = $dom->createAttribute('name');
-    $nameAttribute->value = Mage::app()->getStore()->getFrontendName();
-    $root->appendChild($nameAttribute);
+		$aData['description']= strip_tags($oProduct->short_description);
+		$aData['price'] = preg_replace('/,/', '.', Mage::helper('tax')->getPrice($oProduct, $_finalPrice, true));
 
-    $urlAttribute = $dom->createAttribute('url');
-    $urlAttribute->value = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
-    $root->appendChild($urlAttribute);
+		$aData['link'] = mb_substr($this->base_url . $oProduct->url_path,0,299,'UTF-8');
+		$aData['image_link_large'] = mb_substr($this->media_url.$oProduct->small_image,0,399,'UTF-8');
 
-    $dom->appendChild($root);
+		if( $this->show_outofstock ) {
+			if( $oProduct->isAvailable() ) {
+				$aData['stock'] = 'Y';
+				$aData['stock_descrip'] = $this->instock_msg;
+			} else {
+				$aData['stock'] = 'N';
+				$aData['stock_descrip'] = $this->nostock_msg;
+			}
+		} else {
+			$aData['stock'] = 'Y';
+			$aData['stock_descrip'] = $this->instock_msg;
+		}
 
-    $dom->save($this->file);
-  }
+		$aData['categoryid'] = array_key_exists('cid', $aCats) ? $aCats['cid'] : '';
+		$aData['category'] = array_key_exists('bread', $aCats) ? $aCats['bread'] : '';
 
-  private function openXML() {
-    $this->xml = new DOMDocument();
-    $this->xml->formatOutput = true;
-    $this->xml->load($this->file);
-  }
+		$aData['color'] = @mb_substr($oProduct->color_value,0,99,'UTF-8');
 
-  private function sanitize($data) {
-    $sanitized = array();
-    foreach($data as $k=>$val){
-      $sanitized[$k] = str_replace($this->BadChars,$this->ReplaceChars,$val);
-    }
-    return $sanitized;
-  }
+		if( $oProduct->type_id == 'configurable' ) {
+				unset($sizes);
+				$parent = Mage::getModel('catalog/product_type_configurable')->setProduct($oProduct);
+				$child = $parent->getUsedProductCollection()->addAttributeToSelect('*')->addFilterByRequiredOptions();
+				foreach($child as $simple_product) {
+					$stock = Mage::getModel('cataloginventory/stock_item')->loadByProduct($simple_product);
+					if( !in_array($simple_product->getResource()->getAttribute('size')->getFrontend()->getValue($simple_product), $this->notAllowed) && $stock->getQty() > 0 ) {
+						$sizes[] = $simple_product->getResource()->getAttribute('size')->getFrontend()->getValue($simple_product);
+					}
+				}
+				if( $sizes && count($sizes) > 0 ) {
+					$aData['size'] = implode(',', $sizes);
+				} else {
+					$aData['size'] = '';
+				}
+		}
+		$this->appendXML($aData);
+	}
 
-  private function getProducts() {
-    $this->oProducts = Mage::getModel('catalog/product')->getCollection();
-    $this->oProducts->addAttributeToFilter('status', 1); //enabled
-    $this->oProducts->addAttributeToFilter('visibility', 4); //catalog, search
-    $this->oProducts->addAttributeToFilter(
-      array(
-        array('attribute'=> $this->attribute, 'eq' => '1'),
-      )
-    ); //skroutz products only
-    $this->oProducts->addAttributeToSelect('*');
-    if( !$this->show_outofstock ) {
-      $this->oProducts->joinField('qty',
-                   'cataloginventory/stock_item',
-                   'qty',
-                   'product_id=entity_id',
-                   '{{table}}.stock_id=1',
-                   'left');
-      $this->oProducts->addAttributeToFilter('qty', array("gt" > 0));
-    }
-    $this->oProdudctIds = $this->oProducts->getAllIds();
-  }
+	private function appendXML($p) {
+		if( substr( $p['image_link_large'], -4 ) == '.jpg' ) {
 
-  private function getProductData($iProduct) {
-    $oProduct = Mage::getModel('catalog/product');
-    $oProduct ->load($iProduct);
+			$product = $this->xml->createElement("product");
+			$this->base_node->appendChild( $product );
 
-    $aCats = $this->getCategories($oProduct);
+			$product->appendChild ( $this->xml->createElement('id', $p['id']) );
+			$product->appendChild ( $this->xml->createElement('mpn', $p['mpn']) );
+			$product->appendChild ( $this->xml->createElement('manufacturer', $p['brand']) );
+			$product->appendChild ( $this->xml->createElement('name', ucwords(htmlspecialchars($p['title']))) );
 
-    $aData = array();
+			$description = $product->appendChild($this->xml->createElement('description'));
+			$description->appendChild($this->xml->createCDATASection( $p['description'] ));
 
-    $aData['id']=$iProduct;
-    $aData['mpn']=mb_substr($oProduct->getSku(),0,99,'UTF-8');
+			$product->appendChild ( $this->xml->createElement('price', $p['price']) );
+			$product->appendChild ( $this->xml->createElement('link', $p['link']) );
+			$product->appendChild ( $this->xml->createElement('image', $p['image_link_large']) );
+			$product->appendChild ( $this->xml->createElement('InStock', $p['stock']) );
+			$product->appendChild ( $this->xml->createElement('Availability', $p['stock_descrip']) );
 
-    $aData['brand']=@mb_substr($oProduct->getResource()->getAttribute('manufacturer')->getFrontend()->getValue($oProduct),0,99,'UTF-8');
+			$category = $product->appendChild($this->xml->createElement('category'));
+			$category->appendChild($this->xml->createCDATASection( $p['category'] ));
 
-    $_finalPrice = $oProduct->getFinalPrice();
-    $product_price = Mage::helper('tax')->getPrice($oProduct, $_finalPrice, true);
-    $aData['title']= $aData['brand'] . ' ' . mb_substr($oProduct->getName(),0,299,'UTF-8');
+			$product->appendChild ( $this->xml->createElement('categoryid', $p['categoryid']) );
 
-    $aData['description']= strip_tags($oProduct->getShortDescription());
-    $aData['price'] = preg_replace('/,/', '.', $product_price);
+			if( $p['color'] != '' && !in_array($p['color'], $this->notAllowed) ) {
+				$product->appendChild ( $this->xml->createElement('color', $p['color']) );
+			}
 
-    $aData['link']=mb_substr($oProduct->getProductUrl(),0,299,'UTF-8');
-    $aData['image_link_large']= mb_substr(Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA).'catalog/product'.$oProduct->getImage(),0,399,'UTF-8');
+			if( array_key_exists('size', $p) && $p['size'] != '' ) {
+				$product->appendChild ( $this->xml->createElement('size', $p['size']) );
+			}
 
-    $inventory =  Mage::getModel('cataloginventory/stock_item')->loadByProduct($oProduct);
+		}
+	}
 
-    if( $oProduct->isAvailable() && $inventory->getBackorders() == 0 ) {
-      $aData['stock']='Y';
-      $aData['stock_descrip'] = $this->instock_msg;
-    } elseif( $oProduct->isAvailable() && $inventory->getBackorders() != 0 ) {
-      $aData['stock']='Y';
-      $aData['stock_descrip'] = $this->backorder_msg;
-    } elseif( !$oProduct->isAvailable() ) {
-      $aData['stock']='Y';
-      $aData['stock_descrip'] = $this->nostock_msg;
-    }
+	private function getCategories($oProduct) {
+		$aIds = $oProduct->getCategoryIds();
+		$aCategories = array();
+		$catPath = array();
+		$aCategories['bread'] = '';
 
-    $aData['categoryid'] = $aCats['cid'];
-    $aData['category'] = $aCats['bread'];
+		foreach($aIds as $iCategory){
+			if (!in_array($iCategory, $this->excluded)) {
+			$aCategories['bread'] = '';
+				$oCategory = Mage::getModel('catalog/category')->load($iCategory);
+				$aCategories['cid'] = $oCategory->getId();
+				$aCategories['catpath'] = $oCategory->getPath();
+				$catPath = explode('/', $aCategories['catpath']);
+				foreach($catPath as $cpath){
+					$pCategory = Mage::getModel('catalog/category')->load($cpath);
+					if($pCategory->getName() !='Root Catalog' && $pCategory->getName()!='Default Category'&& $pCategory->getName()!='ΚΑΤΗΓΟΡΙΕΣ'&& $pCategory->getName()!=''){
+						if (!in_array($pCategory->getId(), $this->excluded)) {
+							$aCategories['bread'] .= $pCategory->getName() . ' > ';
+						}
+					}
+				}
+				$aCategories['bread'] = mb_substr(trim(substr($aCategories['bread'],0,-3)),0,299,'UTF-8');
+			}
+		}
 
-    $aData['color']=@mb_substr($oProduct->getResource()->getAttribute('color')->getFrontend()->getValue($oProduct),0,99,'UTF-8');
-
-    if( $oProduct->isConfigurable() ) {
-        unset($sizes);
-        $parent = Mage::getModel('catalog/product_type_configurable')->setProduct($oProduct);
-        $child = $parent->getUsedProductCollection()->addAttributeToSelect('*')->addFilterByRequiredOptions();
-        foreach($child as $simple_product) {
-          if( !in_array($simple_product->getResource()->getAttribute('size')->getFrontend()->getValue($simple_product), $this->notAllowed) )
-            $sizes[] = $simple_product->getResource()->getAttribute('size')->getFrontend()->getValue($simple_product);
-        }
-        if( count($sizes) > 0 ) {
-          $aData['size'] = implode(',', $sizes);
-        } else {
-          $aData['size'] = '';
-        }
-    }
-    return $aData;
-  }
-
-  private function getCategories($oProduct) {
-    $aIds = $oProduct->getCategoryIds();
-    $aCategories = array();
-    $catPath = array();
-    $aCategories['bread'] = '';
-
-    foreach($aIds as $iCategory){
-      if (!in_array($iCategory, $this->excluded)) {
-      $aCategories['bread'] = '';
-        $oCategory = Mage::getModel('catalog/category')->load($iCategory);
-        $aCategories['cid'] = $oCategory->getId();
-        $aCategories['catpath'] = $oCategory->getPath();
-        $catPath = explode('/', $aCategories['catpath']);
-        foreach($catPath as $cpath){
-          $pCategory = Mage::getModel('catalog/category')->load($cpath);
-          if($pCategory->getName() !='Root Catalog' && $pCategory->getName()!='Default Category'&& $pCategory->getName()!='ΚΑΤΗΓΟΡΙΕΣ'&& $pCategory->getName()!=''){
-            if (!in_array($pCategory->getId(), $this->excluded)) {
-              $aCategories['bread'] .= $pCategory->getName() . ' > ';
-            }
-          }
-        }
-        $aCategories['bread'] = mb_substr(trim(substr($aCategories['bread'],0,-3)),0,299,'UTF-8');
-      }
-      }
-
-      return $aCategories;
-  }
+		return $aCategories;
+	}
 
 }
